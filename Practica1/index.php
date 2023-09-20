8d5f314d840afd78746988643497dcdc264ce965
Jose <joseangelgmj@hotmail.com>
2023-09-20 14:44:07 +0200

    practica2

--- Practica1/index.php
+++ Practica1/index.php
@@ -14,7 +14,7 @@
 
         <p>
             <label for="nombre">Nombre</label>
-            <br/>
+            <br />
             <input type="text" id="nombre" name="nombre"></input>
         </p>
 
@@ -22,51 +22,52 @@
 
         <p>
             <label for="apellidos">Apellidos</label>
-            <br/>
+            <br />
             <input type="text" id="apellidos" size="50" name="apellidos"></input>
         </p>
-    
+
         <p>
             <label for="contraseña">Contraseña</label>
-            <br/>
+            <br />
             <input type="password" id="contraseña" name="contraseña"></input>
 
         </p>
 
         <p>
             <label for="dni">Dni</label>
-            <br/>
+            <br />
             <input type="text" size="10" id="dni" name="dni"></input>
         </p>
 
         <p>
             <label for="sexo">Sexo</label>
-            <br/>
+            <br />
             <input type="radio" name="sexo" value="hombre"></input><label for="hombre">Hombre</label>
             <input type="radio" name="sexo" value="mujer"></input><label for="mujer">Mujer</label>
         </p>
 
 
         <p>
-            incluir mi foto: <input type="file" id="image" accept="image/*" name="image" src=""></input>
+            <label for="image">incluir mi foto:</label>
+            <input type="file" id="image" accept="image/*" name="image" src=""></input>
         </p>
 
         <p>
-            Nacido en:
-            <select name="pais">
-                <option value="malaga">Malaga</option>
+            <label for="provincia">Nacido en:</label>
+            <select name="provincia" id="provincia">
+                <option value="malaga" selected>Malaga</option>
                 <option value="cadiz">Cadiz</option>
                 <option value="almeria">Almeria</option>
-
             </select>
         </p>
 
         <p>
-            Comentarios: <textarea name="comentarios" id="comentarios" cols="50" rows="10"></textarea>
+            <label for="comentarios">Comentarios:</label>
+            <textarea name="comentarios" id="comentarios" cols="50" rows="10"></textarea>
         </p>
 
         <p>
-            <input type="checkbox" name="suscrito">Subscribirse al boletín de novedades</input>
+            <input type="checkbox" name="suscrito"><label for="suscrito">Subscribirse al boletín de novedades</label></input>
         </p>
 
 
--- Practica1/recogida.php
+++ Practica1/recogida.php
@@ -1,11 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>recogida datos</h1>
-</body>
-</html>
+<?php
+// primero de todo comprobar que existe un formulario
+if (isset($_POST["guardar"])) {
+
+    // romper el php para incrustar html
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+
+    <body>
+        <h1>Recogida datos</h1>
+        <?php
+
+        // creacion de un array
+        $a[0] = 3;
+        $a[1] = 6;
+        $a[2] = -1;
+        $a[3] = 8;
+
+        // count es el .length del array
+        for ($i = 0; $i < count($a); $i++) {
+            echo "<p>Numero: " . $a[$i] . "</p>";
+        }
+
+
+
+        // para recoger los datos, al enviar el formulario, se reciben aqui como un array
+        // para acceder a ese array que se ha creado, se utilizara $_GET ó $_POST
+    
+        // existen arrays asociativos que se accede al array mediante el nombre del campo
+        // que hemos puesto en el formulario (name="nombre") se accede ($_GET["nombre"] $_POST["nombre"])
+    
+        // accediendo a cada dato de $post
+        echo "<p><strong>Nombre: </strong>" . $_POST["nombre"] . "</p>";
+        echo "<p><strong>Apellidos: </strong>" . $_POST["apellidos"] . "</p>";
+        echo "<p><strong>Clave: </strong>" . $_POST["contraseña"] . "</p>";
+        echo "<p><strong>Nacido en: </strong>" . $_POST["provincia"] . "</p>";
+        echo "<p><strong>Comentarios al respecto: </strong>" . $_POST["comentarios"] . "</p>";
+
+        // acceso a un radiobutton controlando que no se selecciona nada
+        if (isset($_POST["sexo"])) {
+            echo "<p><strong>Sexo: </strong>" . $_POST["sexo"] . "</p>";
+        } else {
+            echo "<p><strong>Sexo: </strong> No Seleccionado </p>";
+        }
+
+        // acceso a un checkbox de boletin
+        if (isset($_POST["suscrito"])) {
+            echo "<p><strong>Suscrito al boletin: </strong> Sí </p>";
+        } else {
+            echo "<p><strong>Suscrito al boletin: </strong> No </p>";
+        }
+        ?>
+    </body>
+
+
+    </html>
+<?php
+}
+
+// si no existe, enviar a la pagina inicial
+else {
+    header("location:index.php");
+}
+?>
