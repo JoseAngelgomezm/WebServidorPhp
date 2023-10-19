08236a8c0b661d82e0cbf28a3ee1799391c56e51
Jose <joseangelgmj@hotmail.com>
2023-10-19 14:33:47 +0200

    termina ej6 y ej practica de examen

---
+++ EjercicioPacticaExamen.php
@@ -0,0 +1,88 @@
+<?php
+
+
+// control errores
+if (isset($_POST["validar"])) {
+
+    $errorCampo = $_POST["palabra"] == "" || is_numeric($_POST["palabra"]) || strlen($_POST["palabra"]) < 2;
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Practica Examen 1</h1>
+    <form action="#" method="post">
+        <p>
+            <label for="palabra">Introduce una palabra</label>
+            <input type="text" name="palabra" id="palabra" value="<?php if (isset($_POST["validar"]))
+                echo $_POST["palabra"] ?>"></input>
+                <?php
+            if (isset($_POST["validar"]) && $errorCampo) {
+                if (is_numeric($_POST["palabra"])) {
+                    echo "<span class='error'>Pon una palabra</span>";
+                } else if(strlen($_POST["palabra"]) < 2){
+                    echo "<span class='error'>Minimo 2 caracteres</span>";
+                }else{
+                    echo "<span class='error'>Campo vacio</span>";
+                }
+
+            }
+            ?>
+        </p>
+
+        <p>
+            <button type="submit" name="validar" id="validar">Validar</button>
+        </p>
+
+        <?php
+        if (isset($_POST["validar"]) && !$errorCampo) {
+            $seRepite = false;
+            $palabra = $_POST["palabra"];
+            // recorrer la palabra completa
+            for ($i = 0; $i < strlen($palabra); $i++) {
+                // quedarnos con la letra
+                $letra = $palabra[$i];
+                // recorrer la palabra desde la posicion siguiente de la letra que tenemos 
+                for ($j = $i+1; $j < strlen($palabra); $j++) {
+                    // si la letra que tenemos se repite en alguna de las posiciones
+                    if ($letra == $palabra[$j]) {
+                        $seRepite = true;
+                        break;
+                    }
+                    
+                }
+                // pararlo si no se ha iterado entero
+                if($j<strlen($palabra)){
+                    break;
+                }
+            }
+
+            if($seRepite){
+                echo "<span class='error'>Contiene caracteres repetidos</span>";
+            }else{
+                echo "<span class='resultado'>NO ontiene caracteres repetidas</span>";
+            }
+        }
+        
+        ?>
+    </form>
+</body>
+<style>
+    .error {
+        color: red;
+    }
+    .resultado{
+        color:green;
+    }
+</style>
+
+</html>
--- Ejercicios_ficheros/Ejercicio6.php
+++ Ejercicios_ficheros/Ejercicio6.php
@@ -15,16 +15,73 @@
     if (!$fd) {
         die("<p>no se ha podido abrir el archivo</p>");
     }
-    ?>
-    <form action="#" method="post">
-        <select name="paises">
-        
-        </select>
-    </form>
-    <?php
+
+    // saltar la primera linea
+    $primeraFila = fgets($fd);
+    while ($linea = fgets($fd)) {
+        $arrayLineas = explode("\t", $linea);
+        $arrayPartesLinea = explode(",", $arrayLineas[0]);
+        $zonas[] = $arrayPartesLinea[2];
+        // preguntar por lo que trae el select , si es lo que hemos seleccionado, añadirle los datos a arrayDatosPaisSeleccionado
+        if (isset($_POST["paises"]) && $_POST["paises"] == $arrayPartesLinea[2]) {
+            $arrayDatosPaisSeleccionado = $arrayLineas;
+        }
+    }
     fclose($fd);
     ?>
 
+    <form action="#" method="post">
+        <p>
+            <label for="pasies">Selecciona una zona</label>
+            <select name="paises">
+                <?php
+                for ($i = 0; $i < count($zonas); $i++) {
+                    if (isset($_POST["paises"]) && $_POST["paises"] == $zonas[$i]) {
+                        echo "<option selected value'" . $zonas[$i] . "'>" . $zonas[$i] . "</option>";
+                    } else {
+                        echo "<option value'" . $zonas[$i] . "'>" . $zonas[$i] . "</option>";
+                    }
+
+                }
+                ?>
+            </select>
+        </p>
+        <p>
+            <button type="submit" name="buscar" id="buscar">Buscar</button>
+        </p>
+    </form>
+    <?php
+    if (isset($_POST["buscar"])) {
+        echo "<h2>PIB PER CAPITA de " . $_POST["paises"] . "</h2>";
+        // dividir la primera fila por tabulador para obtener cada uno de los años
+        $datosPrimeraFila = explode("\t", $primeraFila);
+        // saber cuantas posiciones tiene para poner tantos td como posiciones
+        $numeroAños = count($datosPrimeraFila) - 1;
+
+        echo "<table border='1px'>";
+        echo "<tr>";
+        // empezar por la 1 porque no nos sirve la posicion 0
+        for ($i = 1; $i <= $numeroAños; $i++) {
+            echo "<th>";
+            echo $datosPrimeraFila[$i];
+            echo "</th>";
+        }
+        echo "</tr>";
+
+        echo "<tr>";
+        for ($i = 1; $i <= $numeroAños; $i++) {
+            if(isset($arrayDatosPaisSeleccionado[$i])){
+                echo "<td>".$arrayDatosPaisSeleccionado[$i]."</td>"; 
+            }else{
+                echo "<td></td>";
+            }
+        }
+        echo "</tr>";
+        echo "</table>";
+    }
+
+
+    ?>
 </body>
 
 </html>
