e6c0f518a28ef971c73b24ea57a0bfc46cc5b4ef
Jose Angel <joseangelgmj@hotmail.com>
2023-12-12 21:02:19 +0100

    añade insercion y borrado

--- Ejercicios_Login/PrimerLogin/index.php
+++ Ejercicios_Login/PrimerLogin/index.php
@@ -1,13 +1,70 @@
 <?php
 session_name("PrimerLogin");
 session_start();
-define("TIEMPOMAXIMOSEGUNDOS" , 60);
+define("TIEMPOMAXIMOSEGUNDOS", 600);
 
 // si se ha pulsado salir, destruir la sesion
 if (isset($_POST["salir"])) {
     session_destroy();
     header("location:index.php");
     exit();
+}
+
+if (isset($_POST["botonBorrarUsuario"])) {
+    //conectar a la base de datos
+    try {
+        $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_foro2");
+    } catch (Exception $e) {
+        die("<p>No se ha podido conectar a la base de datos, para el borrado</p></body></html>");
+    }
+
+    // intentar la consulta
+    try {
+        $consulta = "DELETE from usuarios where id_usuario='" . $_POST["botonBorrarUsuario"] . "'";
+        $resultado = mysqli_query($conexion, $consulta);
+    } catch (Exception $e) {
+        mysqli_close($conexion);
+        session_destroy();
+        die(errorPagina("no se ha podido realizar la consulta para comprobar usuario/clave"));
+    }
+
+    mysqli_close($conexion);
+    $_SESSION["mensaje"] = "<p>Se ha borrado con exito el usuario</p>";
+    header("location:index.php");
+    exit();
+}
+
+if (isset($_POST["botonInsertarUsuario"])) {
+    // comprobar los errores
+    $errorNombre = $_POST["nombre"] == "";
+    $errorUsuario = $_POST["usuario"] == "";
+    $errorContraseña = $_POST["contraseña"] == "";
+    $errorEmail = $_POST["email"] == "";
+
+    $errorFormularioInsercion = $errorNombre || $errorUsuario || $errorContraseña || $errorEmail;
+
+    // si no hay errores hacer la insercion
+    if (!$errorFormularioInsercion) {
+        try {
+            $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_foro2");
+        } catch (Exception $e) {
+            die("<p>No se ha podido conectar a la base de datos, para la insercion</p></body></html>");
+        }
+
+        // intentar la consulta
+        try {
+            $consulta = "INSERT INTO `usuarios`(`nombre`, `usuario`, `clave`, `email`, `tipo`) VALUES ('" . $_POST["nombre"] . "','" . $_POST["usuario"] . "','" . md5($_POST["contraseña"]) . "','" . $_POST["email"] . "','" . $_POST["tipo"] . "')";
+            $resultado = mysqli_query($conexion, $consulta);
+        } catch (Exception $e) {
+            mysqli_close($conexion);
+            session_destroy();
+            die(errorPagina("no se ha podido realizar la insercion"));
+        }
+        mysqli_close($conexion);
+        $_SESSION["mensaje"] = "<p>Se ha insertado un usuario correctamente</p>";
+        header("location:index.php");
+        exit();
+    }
 }
 
 // si exsite el session logeado, es que el usuario ya ha iniciado sesion
@@ -30,8 +87,7 @@ if (isset($_SESSION["usuario"])) {
         session_destroy();
         die(errorPagina("no se ha podido realizar la consulta para comprobar usuario/clave"));
     }
-
-
+    // CONTROL SEGURIDAD
     // si no hay tuplas, el usuario ha sido eliminado
     if (mysqli_num_rows($resultado) <= 0) {
         // cerrar conexion
@@ -65,35 +121,129 @@ if (isset($_SESSION["usuario"])) {
 
     // mostrar la pagina para tipo usuario normal o administrador
     if ($datosUsuarioLogeado["tipo"] == "normal") {
-        echo "<h1>Tipo cuenta: normal</h1>";
+      
     } else if ($datosUsuarioLogeado["tipo"] == "admin") {
-        echo "<h1>Tipo cuenta: admin</h1>";
+        ?>
+        <!DOCTYPE html>
+        <html lang="es">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Ejercicio Login</title>
+        </head>
+
+        <body>
+            <h2>Bienvenido
+                <?php echo $_SESSION["usuario"] ?>
+            </h2>
+            <form action="index.php" method="post">
+                <button type="submit" name="salir">Salir</button>
+            </form>
+            <?php
+
+
+            echo "<h1>Tipo cuenta: Administrador</h1>";
+            if (isset($_SESSION["mensaje"])) {
+                echo $_SESSION["mensaje"];
+            }
+            // OBTENER TODOS LOS USUARIOS QUE SEAN NORMALES
+            try {
+                $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_foro2");
+            } catch (Exception $e) {
+                die("<p>No se ha podido conectar a la base de datos, mostrar datos usuarios normales</p></body></html>");
+            }
+
+            // intentar la consulta
+            try {
+                $consulta = "SELECT * from usuarios where tipo='normal'";
+                $resultado = mysqli_query($conexion, $consulta);
+            } catch (Exception $e) {
+                mysqli_close($conexion);
+                session_destroy();
+                die(errorPagina("no se ha podido realizar la consulta para obtener los datos de los usuarios normales"));
+            }
+
+            mysqli_close($conexion);
+
+            echo "<form method='post' action='index.php'>";
+            echo "<table border='1px'>";
+            echo "<tr>";
+            echo "<td>Nombre</td>";
+            echo "<td>Usuario</td>";
+            echo "<td>Email</td>";
+            echo "<td>Tipo</td>";
+            echo "<td><button type='submit' name='botonNuevoUsuario'>Nuevo Ususario+</button></td>";
+            echo "</tr>";
+            while ($datosUsuarios = mysqli_fetch_assoc($resultado)) {
+                echo "<tr>";
+                echo "<td>" . $datosUsuarios["nombre"] . "</td>";
+                echo "<td>" . $datosUsuarios["usuario"] . "</td>";
+                echo "<td>" . $datosUsuarios["email"] . "</td>";
+                echo "<td>" . $datosUsuarios["tipo"] . "</td>";
+                echo "<td><form method='post' action='index.php'><button name='botonBorrarUsuario' value='" . $datosUsuarios["id_usuario"] . "'>Borrar</button></form></td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+            echo "</form>";
+
+            if (isset($_POST["botonNuevoUsuario"]) || isset($_POST["botonInsertarUsuario"]) && $errorFormularioInsercion) {
+                ?>
+                <form action="index.php" method="post">
+                    <br>
+                    <label for="nombre">Nombre: </label><input type="text" name="nombre" value="<?php if (isset($_POST["nombre"]))
+                        echo $_POST["nombre"] ?>">
+                        <?php
+                    if (isset($errorFormularioInsercion) && $errorNombre) {
+                        echo "<span>El nombre esta vacio</span>";
+                    }
+                    ?>
+                    <br>
+                    <br>
+                    <label for="usuario">Usuario: </label><input type="text" name="usuario" value="<?php if (isset($_POST["usuario"]))
+                        echo $_POST["usuario"] ?>">
+                        <?php
+                    if (isset($errorFormularioInsercion) && $errorUsuario) {
+                        echo "<span>El usuario esta vacio</span>";
+                    }
+                    ?>
+                    <br>
+                    <br>
+                    <label for="contraseña">Contraseña: </label><input type="password" name="contraseña" value="">
+                    <?php
+                    if (isset($errorFormularioInsercion) && $errorContraseña) {
+                        echo "<span>La contraseña esta vacia</span>";
+                    }
+                    ?>
+                    <br>
+                    <br>
+                    <label for="email">Email: </label><input type="email" name="email" value="<?php if (isset($_POST["email"]))
+                        echo $_POST["email"] ?>">
+                        <?php
+                    if (isset($errorFormularioInsercion) && $errorEmail) {
+                        echo "<span>El email esta vacio</span>";
+                    }
+                    ?>
+                    <br>
+                    <br>
+                    <label for="">Tipo Usuario: </label>
+                    <label for="normal">Normal</label><input type="radio" name="tipo" id="normal" value="normal">
+                    <label for="administrador">Administrador</label><input type="radio" name="tipo" id="administrador" value="admin">
+                    <br>
+                    <br>
+                    <button type="submit" name="botonInsertarUsuario">Insertar</button>
+                </form>
+                <?php
+            }
+
+            ?>
+        </body>
+
+        </html>
+        <?php
+
+
     }
-
-    ?>
-    <!DOCTYPE html>
-    <html lang="es">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Ejercicio Login</title>
-    </head>
-
-    <body>
-        <h2>Bienvenido
-            <?php echo $_SESSION["usuario"] ?>
-        </h2>
-        <form action="index.php" method="post">
-            <button name="salir">Salir</button>
-            </from>
-    </body>
-
-    </html>
-
-
-    <?php
-
 
     // sino, mostrar el formulario de login
 } else {
@@ -215,7 +365,7 @@ if (isset($_SESSION["usuario"])) {
         }
 
         // si se ha transcurrido el tiempo definido para la session
-        if(isset($_SESSION["inactividad"])){
+        if (isset($_SESSION["inactividad"])) {
             echo "<p class='error'>" . $_SESSION["inactividad"] . "</p>";
             // para eliminar este $_SESSION inactividad
             session_destroy();
