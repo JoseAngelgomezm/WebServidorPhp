5b546578f438a1fa90ca717125aa0a2b3f1cd1c0
Jose Angel <joseangelgmj@hotmail.com>
2023-12-13 14:00:26 +0100

    modifica

--- Ejercicios_Login/PrimerLogin/index.php
+++ Ejercicios_Login/PrimerLogin/index.php
@@ -1,4 +1,19 @@
 <?php
+function errorPagina($mensaje)
+{
+    echo " <!DOCTYPE html>
+<html lang='es'>
+<head>
+ <meta charset='UTF-8'>
+ <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+ <title>Error ejercicio login</title>
+</head>
+<body>
+ <p>" . $mensaje . "</p>
+</body>
+</html>";
+}
+
 session_name("PrimerLogin");
 session_start();
 define("TIEMPOMAXIMOSEGUNDOS", 600);
@@ -121,126 +136,138 @@ if (isset($_SESSION["usuario"])) {
 
     // mostrar la pagina para tipo usuario normal o administrador
     if ($datosUsuarioLogeado["tipo"] == "normal") {
-      
-    } else if ($datosUsuarioLogeado["tipo"] == "admin") {
         ?>
         <!DOCTYPE html>
-        <html lang="es">
+        <html lang="en">
 
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Ejercicio Login</title>
+            <title>Document</title>
         </head>
 
         <body>
-            <h2>Bienvenido
-                <?php echo $_SESSION["usuario"] ?>
-            </h2>
-            <form action="index.php" method="post">
-                <button type="submit" name="salir">Salir</button>
-            </form>
             <?php
+    } else if ($datosUsuarioLogeado["tipo"] == "admin") {
+        ?>
+                <!DOCTYPE html>
+                <html lang="es">
 
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Ejercicio Login</title>
+                </head>
 
-            echo "<h1>Tipo cuenta: Administrador</h1>";
-            if (isset($_SESSION["mensaje"])) {
-                echo $_SESSION["mensaje"];
-            }
-            // OBTENER TODOS LOS USUARIOS QUE SEAN NORMALES
-            try {
-                $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_foro2");
-            } catch (Exception $e) {
-                die("<p>No se ha podido conectar a la base de datos, mostrar datos usuarios normales</p></body></html>");
-            }
-
-            // intentar la consulta
-            try {
-                $consulta = "SELECT * from usuarios where tipo='normal'";
-                $resultado = mysqli_query($conexion, $consulta);
-            } catch (Exception $e) {
-                mysqli_close($conexion);
-                session_destroy();
-                die(errorPagina("no se ha podido realizar la consulta para obtener los datos de los usuarios normales"));
-            }
-
-            mysqli_close($conexion);
-
-            echo "<form method='post' action='index.php'>";
-            echo "<table border='1px'>";
-            echo "<tr>";
-            echo "<td>Nombre</td>";
-            echo "<td>Usuario</td>";
-            echo "<td>Email</td>";
-            echo "<td>Tipo</td>";
-            echo "<td><button type='submit' name='botonNuevoUsuario'>Nuevo Ususario+</button></td>";
-            echo "</tr>";
-            while ($datosUsuarios = mysqli_fetch_assoc($resultado)) {
-                echo "<tr>";
-                echo "<td>" . $datosUsuarios["nombre"] . "</td>";
-                echo "<td>" . $datosUsuarios["usuario"] . "</td>";
-                echo "<td>" . $datosUsuarios["email"] . "</td>";
-                echo "<td>" . $datosUsuarios["tipo"] . "</td>";
-                echo "<td><form method='post' action='index.php'><button name='botonBorrarUsuario' value='" . $datosUsuarios["id_usuario"] . "'>Borrar</button></form></td>";
-                echo "</tr>";
-            }
-            echo "</table>";
-            echo "</form>";
-
-            if (isset($_POST["botonNuevoUsuario"]) || isset($_POST["botonInsertarUsuario"]) && $errorFormularioInsercion) {
-                ?>
-                <form action="index.php" method="post">
-                    <br>
-                    <label for="nombre">Nombre: </label><input type="text" name="nombre" value="<?php if (isset($_POST["nombre"]))
-                        echo $_POST["nombre"] ?>">
-                        <?php
-                    if (isset($errorFormularioInsercion) && $errorNombre) {
-                        echo "<span>El nombre esta vacio</span>";
-                    }
-                    ?>
-                    <br>
-                    <br>
-                    <label for="usuario">Usuario: </label><input type="text" name="usuario" value="<?php if (isset($_POST["usuario"]))
-                        echo $_POST["usuario"] ?>">
-                        <?php
-                    if (isset($errorFormularioInsercion) && $errorUsuario) {
-                        echo "<span>El usuario esta vacio</span>";
-                    }
-                    ?>
-                    <br>
-                    <br>
-                    <label for="contraseña">Contraseña: </label><input type="password" name="contraseña" value="">
+                <body>
+                    <h2>Bienvenido
+                    <?php echo $_SESSION["usuario"] ?>
+                    </h2>
+                    <form action="index.php" method="post">
+                        <button type="submit" name="salir">Salir</button>
+                    </form>
                     <?php
-                    if (isset($errorFormularioInsercion) && $errorContraseña) {
-                        echo "<span>La contraseña esta vacia</span>";
-                    }
-                    ?>
-                    <br>
-                    <br>
-                    <label for="email">Email: </label><input type="email" name="email" value="<?php if (isset($_POST["email"]))
-                        echo $_POST["email"] ?>">
-                        <?php
-                    if (isset($errorFormularioInsercion) && $errorEmail) {
-                        echo "<span>El email esta vacio</span>";
-                    }
-                    ?>
-                    <br>
-                    <br>
-                    <label for="">Tipo Usuario: </label>
-                    <label for="normal">Normal</label><input type="radio" name="tipo" id="normal" value="normal">
-                    <label for="administrador">Administrador</label><input type="radio" name="tipo" id="administrador" value="admin">
-                    <br>
-                    <br>
-                    <button type="submit" name="botonInsertarUsuario">Insertar</button>
-                </form>
-                <?php
-            }
 
-            ?>
-        </body>
 
-        </html>
-        <?php
+                    echo "<h1>Tipo cuenta: Administrador</h1>";
+                    if (isset($_SESSION["mensaje"])) {
+                        echo $_SESSION["mensaje"];
+                    }
+                    // OBTENER TODOS LOS USUARIOS QUE SEAN NORMALES
+                    try {
+                        $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_foro2");
+                    } catch (Exception $e) {
+                        die("<p>No se ha podido conectar a la base de datos, mostrar datos usuarios normales</p></body></html>");
+                    }
+
+                    // intentar la consulta
+                    try {
+                        $consulta = "SELECT * from usuarios where tipo='normal'";
+                        $resultado = mysqli_query($conexion, $consulta);
+                    } catch (Exception $e) {
+                        mysqli_close($conexion);
+                        session_destroy();
+                        die(errorPagina("no se ha podido realizar la consulta para obtener los datos de los usuarios normales"));
+                    }
+
+                    mysqli_close($conexion);
+
+                    echo "<form method='post' action='index.php'>";
+                    echo "<table border='1px'>";
+                    echo "<tr>";
+                    echo "<td>Nombre</td>";
+                    echo "<td>Usuario</td>";
+                    echo "<td>Email</td>";
+                    echo "<td>Tipo</td>";
+                    echo "<td><button type='submit' name='botonNuevoUsuario'>Nuevo Ususario+</button></td>";
+                    echo "</tr>";
+                    while ($datosUsuarios = mysqli_fetch_assoc($resultado)) {
+                        echo "<tr>";
+                        echo "<td>" . $datosUsuarios["nombre"] . "</td>";
+                        echo "<td>" . $datosUsuarios["usuario"] . "</td>";
+                        echo "<td>" . $datosUsuarios["email"] . "</td>";
+                        echo "<td>" . $datosUsuarios["tipo"] . "</td>";
+                        echo "<td><form method='post' action='index.php'><button name='botonBorrarUsuario' value='" . $datosUsuarios["id_usuario"] . "'>Borrar</button></form></td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
+                    echo "</form>";
+
+                    if (isset($_POST["botonNuevoUsuario"]) || isset($_POST["botonInsertarUsuario"]) && $errorFormularioInsercion) {
+                        ?>
+                        <form action="index.php" method="post">
+                            <br>
+                            <label for="nombre">Nombre: </label><input type="text" name="nombre" value="<?php if (isset($_POST["nombre"]))
+                                echo $_POST["nombre"] ?>">
+                            <?php
+                            if (isset($errorFormularioInsercion) && $errorNombre) {
+                                echo "<span>El nombre esta vacio</span>";
+                            }
+                            ?>
+                            <br>
+                            <br>
+                            <label for="usuario">Usuario: </label><input type="text" name="usuario" value="<?php if (isset($_POST["usuario"]))
+                                echo $_POST["usuario"] ?>">
+                            <?php
+                            if (isset($errorFormularioInsercion) && $errorUsuario) {
+                                echo "<span>El usuario esta vacio</span>";
+                            }
+                            ?>
+                            <br>
+                            <br>
+                            <label for="contraseña">Contraseña: </label><input type="password" name="contraseña" value="">
+                            <?php
+                            if (isset($errorFormularioInsercion) && $errorContraseña) {
+                                echo "<span>La contraseña esta vacia</span>";
+                            }
+                            ?>
+                            <br>
+                            <br>
+                            <label for="email">Email: </label><input type="email" name="email" value="<?php if (isset($_POST["email"]))
+                                echo $_POST["email"] ?>">
+                            <?php
+                            if (isset($errorFormularioInsercion) && $errorEmail) {
+                                echo "<span>El email esta vacio</span>";
+                            }
+                            ?>
+                            <br>
+                            <br>
+                            <label for="">Tipo Usuario: </label>
+                            <label for="normal">Normal</label><input type="radio" name="tipo" id="normal" value="normal">
+                            <label for="administrador">Administrador</label><input type="radio" name="tipo" id="administrador"
+                                value="admin">
+                            <br>
+                            <br>
+                            <button type="submit" name="botonInsertarUsuario">Insertar</button>
+                        </form>
+                    <?php
+                    }
+
+                    ?>
+                </body>
+
+                </html>
+            <?php
 
 
     }
@@ -292,88 +319,73 @@ if (isset($_SESSION["usuario"])) {
             }
         }
     }
-
-    function errorPagina($mensaje)
-    {
-        echo " <!DOCTYPE html>
-<html lang='es'>
-<head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Error ejercicio login</title>
-</head>
-<body>
-    <p>" . $mensaje . "</p>
-</body>
-</html>";
-    }
     ?>
 
-    <!DOCTYPE html>
-    <html lang="es">
+        <!DOCTYPE html>
+        <html lang="es">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-        <style>
-            .error {
-                color: red;
-            }
-        </style>
-    </head>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+            <style>
+                .error {
+                    color: red;
+                }
+            </style>
+        </head>
 
-    <body>
-        <h2>Primer Login</h2>
-        <form action="index.php" method="post">
-            <p>
-                <label for="usuario">Usuario: </label>
-                <input type="text" name="usuario" id="usuario" value="<?php if (isset($_POST["usuario"]))
-                    echo $_POST["usuario"] ?>">
+        <body>
+            <h2>Primer Login</h2>
+            <form action="index.php" method="post">
+                <p>
+                    <label for="usuario">Usuario: </label>
+                    <input type="text" name="usuario" id="usuario" value="<?php if (isset($_POST["usuario"]))
+                        echo $_POST["usuario"] ?>">
+                        <?php
+                    if (isset($_POST["usuario"]) && $errorUsuario) {
+                        if ($_POST["usuario"] == "") {
+                            echo "<span class='error'>Campo vacio</span>";
+                        } else if (isset($errorUsuarioNoExiste) && $errorUsuarioNoExiste) {
+                            echo "<span class='error'>El usuario no existe o la clave no es correcta</span>";
+                        }
+                    }
+                    ?>
+                </p>
+
+                <p>
+                    <label for="contraseña">Contraseña: </label>
+                    <input type="password" name="contraseña" id="contraseña" value="">
                     <?php
-                if (isset($_POST["usuario"]) && $errorUsuario) {
-                    if ($_POST["usuario"] == "") {
-                        echo "<span class='error'>Campo vacio</span>";
-                    } else if (isset($errorUsuarioNoExiste) && $errorUsuarioNoExiste) {
-                        echo "<span class='error'>El usuario no existe o la clave no es correcta</span>";
+                    if (isset($_POST["contraseña"]) && $errorContraseña) {
+                        if ($_POST["contraseña"] == "") {
+                            echo "<span class='error'>Campo vacio</span>";
+                        }
                     }
-                }
-                ?>
-            </p>
+                    ?>
+                </p>
+                <p>
+                    <button type="submit" name="botonLogin">Login</button>
+                </p>
+            </form>
+            <?php
+            // si se ha eliminado el usuario y existe session seguridad
+            if (isset($_SESSION["seguridad"])) {
+                echo "<p class='error'>" . $_SESSION["seguridad"] . "</p>";
+                // para eliminar este $_SESSION seguridad
+                session_destroy();
+            }
 
-            <p>
-                <label for="contraseña">Contraseña: </label>
-                <input type="password" name="contraseña" id="contraseña" value="">
-                <?php
-                if (isset($_POST["contraseña"]) && $errorContraseña) {
-                    if ($_POST["contraseña"] == "") {
-                        echo "<span class='error'>Campo vacio</span>";
-                    }
-                }
-                ?>
-            </p>
-            <p>
-                <button type="submit" name="botonLogin">Login</button>
-            </p>
-        </form>
+            // si se ha transcurrido el tiempo definido para la session
+            if (isset($_SESSION["inactividad"])) {
+                echo "<p class='error'>" . $_SESSION["inactividad"] . "</p>";
+                // para eliminar este $_SESSION inactividad
+                session_destroy();
+            }
+            ?>
+        </body>
+
+        </html>
         <?php
-        // si se ha eliminado el usuario y existe session seguridad
-        if (isset($_SESSION["seguridad"])) {
-            echo "<p class='error'>" . $_SESSION["seguridad"] . "</p>";
-            // para eliminar este $_SESSION seguridad
-            session_destroy();
-        }
-
-        // si se ha transcurrido el tiempo definido para la session
-        if (isset($_SESSION["inactividad"])) {
-            echo "<p class='error'>" . $_SESSION["inactividad"] . "</p>";
-            // para eliminar este $_SESSION inactividad
-            session_destroy();
-        }
-        ?>
-    </body>
-
-    </html>
-    <?php
 }
 ?>
