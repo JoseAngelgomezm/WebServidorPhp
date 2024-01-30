f5917703d4484479e0cb1be8f55e0d2962f00edd
Jose Angel <joseangelgmj@hotmail.com>
2024-01-30 14:10:03 +0100

    añade directivas para hacer peticiones axios

--- Ejercicios_API_REST/Ejercicio3/Api/index.php
+++ Ejercicios_API_REST/Ejercicio3/Api/index.php
@@ -1,5 +1,10 @@
 <?php
 
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+
 require __DIR__ . '/Slim/autoload.php';
 
 $app = new \Slim\App;
